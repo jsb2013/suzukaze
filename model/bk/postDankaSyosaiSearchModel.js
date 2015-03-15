42e7d90f2311cd2e2f8ac696a9d6a45398f58f26
sora_kumo_hoshizora <sora_kumo_hoshizora@yahoo.co.jp>
2015-03-15 10:23:29 +0900

    バックへ移動

---
+++ model/bk/postDankaSyosaiSearchModel.js
@@ -0,0 +1,281 @@
+/*
+ * ユーザ待ちステータステーブルに
+ * アクセスする為のクラス
+ */
+ 
+var database = require("../../../dao/database");
+var client = database.createClient();
+var dbcommon = require("../../../dao/database_common");
+var log = require("../../../util/logger");
+var logger = log.createLogger();
+var util = require("../../../util/util");
+var async = require("async");
+var mTikuCodeDao = require("../../../dao/mTikuCodeDao");
+var mMemberDao = require("../../../dao/mMemberDao");
+var mTagsDao = require("../../../dao/mTagsDao");
+var fs = require("fs");
+
+/* 檀家追加画面メイン（post処理） */
+exports.main = function (baseInfoInWeb, callback) {
+
+    var baseInfoInDb = [];
+    var tikuList = [];
+    var tagList = [];
+    var resultInfo = [];
+
+    async.series([
+
+    // 地区コードマスタを取得する
+        function (dbcallback) {
+            mTikuCodeDao.getMTikuCode(client, database, tikuList, dbcallback);
+        },
+    // タグコードマスタを取得する
+        function (dbcallback) {
+            mTagsDao.getMTags(client, database, tagList, dbcallback);
+        },
+    // 檀家検索結果取得（50音検索）
+        function (dbcallback) {
+            mMemberDao.getMmemberAndTDanka(client, database, baseInfoInDb, dbcallback);
+        } ],
+    // 【END】トランザクション完了(commit or rollback)
+        function (err, results) {
+            if (err) {
+                callback(true);
+                return;
+            }
+            // 検索条件により絞込みを行う。
+            getBaseInfoBysearchData(baseInfoInDb, baseInfoInWeb, resultInfo);
+
+            // DB検索条件をcsv形式に変換する。（delimiterは"|"）
+            var stringList = [];
+            createString(resultInfo, stringList);
+
+            // 檀家検索情報をファイル出力する。（→danka_search_info.txt）
+            fileOutDankaInfoToString(stringList);
+
+            var tikuInfoJson = convertTikuListToJson(tikuList);
+            if (!tikuInfoJson) {
+                callback(true);
+            }
+            var tagInfoJson = convertTagListToJson(tagList);
+            if (!tagInfoJson) {
+                callback(true);
+            }
+            var seachTitle = "詳細条件検索";
+            callback(false, resultInfo, tikuInfoJson, tikuList, tagInfoJson, tagList, seachTitle);
+            return;
+        }
+    );
+};
+
+// 檀家検索情報をファイル出力する。（→danka_search_info.txt）
+function fileOutDankaInfoToString(stringList){
+    if(fs.existsSync("./tmp/danka_search_info.txt")){
+        fs.unlinkSync("./tmp/danka_search_info.txt");    
+    }
+    var fd = fs.openSync("./tmp/danka_search_info.txt", "a");
+    for(var key in stringList){
+       var line = stringList[key];
+        fs.writeSync(fd, line, 0);
+    }
+    fs.closeSync(fd);
+}
+
+// DB検索条件をcsv形式に変換する。（delimiterは"|"）
+function createString(resultInfo, stringList){
+    for(var key in resultInfo){
+        var baseInfo = resultInfo[key];
+        var memberId = baseInfo.member_id;
+        var dankaType = baseInfo.danka_type;
+        var nameSei = baseInfo.name_sei;
+        var nameNa = baseInfo.name_na;
+        var tags = baseInfo.tags;
+        var isArive = baseInfo.is_arive;
+        var tikuCode = baseInfo.tiku_code;
+        var memberIdKosyu = baseInfo.member_id_kosyu;
+        var jiin = baseInfo.jiin;
+        var stringLine = memberId + "|" + dankaType + "|" + nameSei + "|" + nameNa + "|" + tags + "|" + isArive + "|" + tikuCode + "|" + memberIdKosyu + "|" + jiin + "\n";
+        stringList.push(stringLine);
+    }
+}
+
+// 指定した条件でレコードを絞り込む。
+function getBaseInfoBysearchData(baseInfoInDb, baseInfoInWeb, resultInfo){
+ 
+    var resultCount = 0;
+    for (var key in baseInfoInDb) {
+        var info = baseInfoInDb[key];
+
+        // 戸主名(性）で検索
+        var nameSeiInDb = info.name_sei;
+        var nameSeiInWeb = baseInfoInWeb.name_sei;
+        if (!checkValueByString(nameSeiInDb, nameSeiInWeb)) {
+            continue;
+        }
+
+        // 戸主名(名）で検索
+        var nameNaInDb = info.name_na;
+        var nameNaInWeb = baseInfoInWeb.name_na;
+        if (!checkValueByString(nameNaInDb, nameNaInWeb)) {
+            continue;
+        }
+
+        // ふりがな(性）で検索
+        var furiganaSeiInDb = info.furigana_sei;
+        var furiganaSeiInWeb = baseInfoInWeb.furigana_sei;
+        if (!checkValueByString(furiganaSeiInDb, furiganaSeiInWeb)) {
+            continue;
+        }
+
+        // ふりがな(名）で検索
+        var furiganaNaInDb = info.furigana_na;
+        var furiganaNaInWeb = baseInfoInWeb.furigana_na;
+        if (!checkValueByString(furiganaNaInDb, furiganaNaInWeb)) {
+            continue;
+        }
+
+        // 地区コードで検索
+        var tikuCodeInDb = info.tiku_code;
+        var tikuCodeInWeb = baseInfoInWeb.tiku_code;
+        if (!checkValueByNumeric(tikuCodeInDb, tikuCodeInWeb)) {
+            continue;
+        }
+
+        // 世話コードで検索
+        var sewaCodeInDb = info.sewa_code;
+        var sewaCodeInWeb = baseInfoInWeb.sewa_code;
+        if (!checkValueByNumeric(sewaCodeInDb, sewaCodeInWeb)) {
+            continue;
+        }
+
+        // 所属寺で検索
+        var jiinInDb = info.jiin;
+        var jiinInWebInfo = baseInfoInWeb.jiin;
+        var isMatch = false;
+        for(var key in jiinInWebInfo){
+            var _jiinInWeb = jiinInWebInfo[key];
+            if (!checkValueByNumeric(jiinInDb, _jiinInWeb)) {
+                continue;
+            }
+            isMatch = true;
+            break;
+        }
+        if(!util.isUndefineForList(jiinInWebInfo) && !isMatch){
+            continue;
+        }
+
+        // タグで検索
+        var tagsInDb = info.tags;
+        var tagsInWebInfo = baseInfoInWeb.tags;
+        var isMatch = false;
+        if(util.isUndefine(tagsInWebInfo) || util.isUndefine(tagsInWebInfo.size)){
+            var tagName = tagsInWebInfo;
+            if (!checkValueByString(tagsInDb, tagName)) {
+                continue;
+            }
+            isMatch = true;
+        } else{
+            for(var key in tagsInWebInfo){
+                var tagName = tagsInWebInfo[key];
+                if(tagsInWebInfo.size == 1){
+                    tagName = tagsInWebInfo;
+                }
+                if (!checkValueByString(tagsInDb, tagName)) {
+                    continue;
+                }
+                isMatch = true;
+                break;
+            }    
+        }
+        
+        if(!util.isUndefineForList(tagsInWebInfo) && !isMatch){
+            continue;
+        }
+
+        // 生死区分で検索
+        var isAriveTypeInDb = info.is_arive;
+        var includeKojinInWeb = baseInfoInWeb.include_kojin;
+        if (util.isUndefine(includeKojinInWeb)){
+            if(isAriveTypeInDb !== 1){
+                continue;
+            }
+        }
+        resultInfo.push(info);
+        //resultInfo[resultCount] = info;
+        //resultCount++;
+    }
+}
+
+function checkValueByString(valueInDb, valueInWeb){
+
+    // DBに定義されている値がない場合は、valueInWebの指定有無で条件が確定する。
+    if(util.isUndefine(valueInDb)){
+        if(util.isUndefine(valueInWeb)){
+            return true;
+        }
+        return false; 
+    }
+
+    // valueInWebが空の場合は、検索キー外としてスキップする。
+    if(util.isUndefine(valueInWeb)){
+        return true; 
+    }
+    // 部分一致する場合は必ず0以上の値になる。(→次の検索条件へ! return true）
+    var index = valueInDb.indexOf(valueInWeb);
+    if(index > -1){
+        return true;
+    }
+    return false;
+}
+
+function checkValueByNumeric(valueInDb, valueInWeb){
+
+    // DBに定義されている値がない場合は、valueInWebの指定有無で条件が確定する。
+    if(util.isUndefine(valueInDb) || valueInDb == 0){
+        if(util.isUndefine(valueInWeb) || valueInWeb == 0){
+            return true;
+        }
+        return false; 
+    }
+
+    // valueInWebが空の場合は、検索キー外としてスキップする。
+    if(util.isUndefine(valueInWeb) || valueInWeb == 0){
+        return true; 
+    }
+    // 一致する場合は次の検索条件へ! return true）
+    if(valueInDb == valueInWeb){
+        return true;
+    }
+    return false;
+}
+
+function convertTikuListToJson(tikuList) {
+    var tikuInfoJson = {};
+    for(var i in tikuList){
+        var tikuCode = tikuList[i].tiku_code;
+        var tikuName = tikuList[i].tiku_name;
+        tikuInfoJson[tikuCode] = tikuName;
+    }
+    return tikuInfoJson;
+}
+
+function convertSewaListToJson(sewaList) {
+    var sewaInfoJson = {};
+    for(var i in sewaList){
+        var sewaCode = sewaList[i].sewa_code;
+        var sewaName = sewaList[i].sewa_name;
+        sewaInfoJson[sewaCode] = sewaName;
+    }
+    return sewaInfoJson;
+}
+
+function convertTagListToJson(tagList) {
+    var tagInfoJson = {};
+    for(var i in tagList){
+        var tagCode = tagList[i].tags_id;
+        var tagName = tagList[i].tags;
+        tagInfoJson[tagCode] = tagName;
+    }
+    return tagInfoJson;
+}
+
---
+++ model/danka_detail/kihon/getDankaDetailKihonModel.js
@@ -0,0 +1,170 @@
+/*
+ * ユーザ待ちステータステーブルに
+ * アクセスする為のクラス
+ */
+ 
+var database = require("../../../dao/database");
+var client = database.createClient();
+var log = require("../../../util/logger");
+var logger = log.createLogger();
+var util = require("../../../util/util");
+var async = require('async');
+var tDankaDetailKosyuDao = require("../../../dao/tDankaDetailKosyuDao");
+var mCommentDao = require("../../../dao/mCommentDao");
+var mMemberDao = require("../../../dao/mMemberDao");
+var mSewaCodeDao = require("../../../dao/mSewaCodeDao");
+var mTikuCodeDao = require("../../../dao/mTikuCodeDao");
+var mAddressDao = require("../../../dao/mAddressDao");
+var mMailDao = require("../../../dao/mMailDao");
+var mTelnumberDao = require("../../../dao/mTelnumberDao");
+var mTagsDao = require("../../../dao/mTagsDao");
+
+/* 檀家追加画面メイン（post処理） */
+exports.main = function (memberId, callback) {
+    // いったんはpostで入ってきたデータは正しい想定で作る
+    //var memberId = webItemJson.member_id;
+    var resultRows = [];
+    var tikuCodeInfo = [];
+    var sewaCodeInfo = [];
+    var addressInfo = [];
+    var mailInfo = [];
+    var commentInfo = [];
+    var telnumberInfo = [];
+    var kosyuInfo = [];
+    var tagsInfo = [];
+
+    async.series([
+
+    // 檀家検索結果取得（50音検索）
+        function (dbcallback) {
+            mMemberDao.getMmemberAndTDankaByMemberId(client, database, memberId, resultRows, dbcallback);
+        },
+    // 地区コードマスタを取得
+        function (dbcallback) {
+            mTikuCodeDao.getMTikuCode(client, database, tikuCodeInfo, dbcallback);
+        },
+    // 世話コードマスタを取得
+        function (dbcallback) {
+            mSewaCodeDao.getMSewaCode(client, database, sewaCodeInfo, dbcallback);
+        },
+    // 住所マスタを取得
+        function (dbcallback) {
+            mAddressDao.getAddressInfoByMemberIdAndPriority(client, database, memberId, 1, addressInfo, dbcallback);
+        },
+    // メールマスタを取得
+        function (dbcallback) {
+            mMailDao.getMailInfoByMemberId(client, database, memberId, mailInfo, dbcallback);
+        },
+    // 電話番号マスタを取得
+        function (dbcallback) {
+            mTelnumberDao.getTelnumberInfoByMemberId(client, database, memberId, telnumberInfo, dbcallback);
+        },
+    // T_xxを削除
+        function (dbcallback) {
+            tDankaDetailKosyuDao.deleteTDankaDetailKosyuBymemberId(client, database, memberId, dbcallback);
+        },
+    // T_xxにインサート
+        function (dbcallback) {
+            // 地区名と世話名をresultRowsにAdd
+            addTikuNameAndSewaName2(tikuCodeInfo, sewaCodeInfo, resultRows);
+            // メイン処理
+            tDankaDetailKosyuDao.insertTDankaDetailKosyuInfo(client, database, memberId, resultRows[0], dbcallback);
+        },
+    // T_xxxマスタを取得
+        function (dbcallback) {
+            tDankaDetailKosyuDao.getTDankaDetailKosyuInfoByMemberId(client, database, memberId, kosyuInfo, dbcallback);
+        },
+    // コメントマスタを取得
+        function (dbcallback) {
+            mCommentDao.getTCommentByMemberId(client, database, memberId, commentInfo, dbcallback);
+        },
+    // タグ情報を取得（戸主情報）
+        function (dbcallback) {
+            mTagsDao.getMTags(client, database, tagsInfo, dbcallback);
+        } ],
+    // 【END】トランザクション完了(commit or rollback)
+        function (err, results) {
+            if (err) {
+                callback(true);
+                return;
+            }
+
+            // 住所情報をpriority順に並び替える。
+            convertInfoByPriority(addressInfo);
+
+            // メール情報をpriority順に並び替える。
+            convertInfoByPriority(mailInfo);
+
+            // 電話番号情報をpriority順に並び替える。
+            convertInfoByPriority(telnumberInfo);
+
+            // 仕事名称とコメントをwebitemJsonに登録
+            // ※本内容は「danka_detail_kihon」内では使用しないが、その後「danka_detail_kihon_confirm」で使用する為、
+            registerNama(kosyuInfo, commentInfo);
+
+            var tagNameListInMM = util.splitStringByDelimiter(kosyuInfo[0].tags, ",");
+
+            callback(false, kosyuInfo, tikuCodeInfo, sewaCodeInfo, addressInfo, mailInfo, telnumberInfo, tagsInfo, tagNameListInMM);
+            return;
+        }
+    );
+};
+
+function registerNama(kosyuInfo, commentInfo){
+    // コメントを登録
+    var comment = "";
+    if(!util.isUndefineForList(commentInfo)){
+        comment = commentInfo[0].comment;
+    }
+    kosyuInfo[0].comment = comment;
+}
+
+function convertInfoByPriority(infoJson){
+    var priorityInfo1 = {};
+    var priorityInfo2 = {};
+    var priorityInfo3 = {};
+    
+    for(var key in infoJson){
+        var line = infoJson[key];
+        var priority = line.priority;
+        if(priority === 1){
+            priorityInfo1 = line;
+            continue;
+        }
+        if(priority === 2){
+            priorityInfo2 = line;
+            continue;
+        }
+        if(priority === 3){
+            priorityInfo3 = line;
+            continue;
+        }
+    }
+    infoJson[0] = priorityInfo1;
+    infoJson[1] = priorityInfo2;
+    infoJson[2] = priorityInfo3;
+}
+
+function addTikuNameAndSewaName2(tikuCodeInfo, sewaCodeInfo, resultRows) {
+    var _tikuCode = resultRows[0].tiku_code;
+    var _sewaCode = resultRows[0].sewa_code;
+
+    for(var i in tikuCodeInfo){
+        var tc = tikuCodeInfo[i].tiku_code;
+        if(_tikuCode == tc){
+            var tn = tikuCodeInfo[i].tiku_name;
+            resultRows[0].tiku_name = tn;
+            break;
+        }
+    }
+
+    for(var i in sewaCodeInfo){
+        var sc = sewaCodeInfo[i].sewa_code;
+        if(_sewaCode == sc){
+            var sn = sewaCodeInfo[i].sewa_name;
+            resultRows[0].sewa_name = sn;
+            break;
+        }
+    }
+    return;
+}
